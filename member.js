8651d0bf82e9a18ff85a7b376099ca3616ec3e15
francescopollice <159261777+francescopollice@users.noreply.github.com>
2025-01-09 17:51:33 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+  return (
+    <div>
+      <h1>Member Skills</hjson>
+      <p>JavaScript</p>
+      <p>NodeJS</p>
+      <p>React</p>
+      <p>HTML</p>
+      <p>CSS</p>
+    </div>
+  );
+}
